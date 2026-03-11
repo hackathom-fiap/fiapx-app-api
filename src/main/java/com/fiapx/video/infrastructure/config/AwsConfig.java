9c87a54ade0d0ae97b2a4cdfa9d3e5ad3ee1476c
hmouraf <humbertomoura@gmail.com>
2026-03-10 23:22:59 -0300

    fix sonar

--- src/main/java/com/fiapx/video/infrastructure/config/AwsConfig.java
+++ src/main/java/com/fiapx/video/infrastructure/config/AwsConfig.java
@@ -10,6 +10,7 @@ public class AwsConfig {
 
     @Bean
     public S3Client s3Client() {
+        System.out.println("--- CONFIGURANDO S3 CLIENT NA REGIAO: US_EAST_1");
         return S3Client.builder()
                 .region(Region.US_EAST_1)
                 .build();
